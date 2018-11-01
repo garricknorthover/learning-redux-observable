f6414c2e6f82eefb89777a86c040377192ef828c
garricknorthover <garricknorthover@gmail.com>
2018-11-02 11:27:57 +1300

    rxjs-marbles works but going to try jest-marbles

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,13 +1,12 @@
-import { mapTo, delay } from 'rxjs/operators'
+import { mapTo, delay, takeUntil } from 'rxjs/operators'
 import { ofType } from 'redux-observable'
 import { combineEpics } from 'redux-observable'
 
 const enteringTextEpic = action$ =>
   action$.pipe(
     ofType('SET_TODO'),
-    delay(2),
-    mapTo({ type: 'SET_MESSAGE', text: 'you are entering text what what!' }),
-    
+    delay(1000),
+    mapTo({ type: 'SET_MESSAGE', text: 'you are entering text' }),
   )
 
 const epica = combineEpics(enteringTextEpic)
--- src/epics/index.spec.js
+++ src/epics/index.spec.js
@@ -1,18 +1,21 @@
 import { marbles } from 'rxjs-marbles/jest'
 import enteringTextEpic from './index'
 
-it(
-  'should just feckin work',
-  marbles(m => {
-    const values = {
-      a: { type: 'SET_TODO' },
-      b: { type: 'SET_MESSAGE', text: 'you are entering text what what!' }
-    }
-    const action$ = m.cold('a----', values)
-    const expected$ = m.cold('--b', values)
 
-    const actual$ = enteringTextEpic(action$)
+describe('entering text epic', () => {
+  it(
+    'should just feckin work',
+    marbles(m => {
+      const values = {
+        a: { type: 'SET_TODO' },
+        b: { type: 'SET_MESSAGE', text: 'you are entering text' }
+      }
 
-    m.expect(actual$).toBeObservable(expected$)
-  })
-)
+      const action$ = m.cold('- a        --|', values)
+      const expected$ = m.cold('- 1000ms b --|', values)
+      const actual$ = enteringTextEpic(action$)
+
+      m.expect(actual$).toBeObservable(expected$)
+    })
+  )
+})
