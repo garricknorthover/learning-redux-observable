71097faff0a02ea33c7a873dbc00fd97644e459c
garricknorthover <garricknorthover@gmail.com>
2018-12-18 22:43:02 +1300

    nope not there yet

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,18 +1,21 @@
 import { ofType } from 'redux-observable'
-import { of, from, concat } from 'rxjs'
+import { of, from, concat, pipe } from 'rxjs'
 import { delay, merge, map, flatMap } from 'rxjs/operators'
 import { combineEpics } from 'redux-observable'
 
 const enteringTextEpic = action$ =>
   action$.pipe(
     ofType('SET_TODO'),
+    delay(3000),
     flatMap(action =>
       concat(
         of({ type: 'SET_MESSAGE', text: 'you are entering text' }),
-        of({ type: 'SET_MESSAGE', text: 'not' })
+        of(delay(3000)),
+        of({ type: 'SET_MESSAGE', text: 'you are not entering text' })
       )
     )
   )
+
 // const messageEpic = action$ =>
 //   action$.pipe(
 //     ofType('SET_MESSAGE'),
